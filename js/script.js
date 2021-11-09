83f0d8826cb9137d408d5a637e42431f3bc58400
callmejogi <86162176+callmejogi@users.noreply.github.com>
2021-11-09 16:38:58 +0200

    final

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 // const arr = [1,2,3,4,5,'Jora'];
-const arr = ['Jora','Tom','Paul','Rob','Fox'];
+
 
 // function indexOf(arr,val,fromIndex = 0){
 //     if (fromIndex >= arr.length) return -1;
@@ -31,23 +31,77 @@ const arr = ['Jora','Tom','Paul','Rob','Fox'];
 // console.log(lastIndexOf(arr,1,-2));
 // console.log(lastIndexOf(arr,5,10));
 
-const cb = function(element,index,array){
-    for(let i = 0; i <= arr.length ;i++){
-        if (index === arr[i]) return true;
-        if (array[i] === element) return element;
+const arr = ['Jora','Tom','Paul','Rob','Fox'];
+
+// function find(arr,callback){
+//     for(let i = 0; i < arr.length ;i++){
+        
+//         if (callback(arr[i],i,arr)) return arr[i];
+//     }
+// }
+
+// console.log(find(
+//     arr,
+//     item => item === 'Fox'
+// ))
+
+// const findIndex = (arr,callback) => {
+//     for(let i = 0; i < arr.length ;i++){
+//         if (callback(arr[i],i,arr)) return i;
+//     }
+
+//     return -1;
+// }
+
+// console.log(findIndex(
+//     arr,
+//     item => item === 'Jora'
+// ))
+
+
+// const includesItem = (arr,searchElement,fromIndex = 0) => {
+//     if (fromIndex >= arr.length) return false;
+
+//     for(let i = fromIndex; i <= arr.length ;i++){
+//         if (arr[i] === searchElement ) return true;
+//     }
+
+//     return false;
+// }
+
+// console.log(includesItem(arr,'Tom'));
+// console.log(includesItem(arr,'Jimmy'));
+// console.log(includesItem(arr,'Tom',7));
+
+// function some(arr,callback){
+//     for(let i = 0;i < arr.length ; i++){
+//         if (callback(arr[i],i,arr)) return true;
+//     }
+
+//     return false
+// }
+
+// console.log(some(arr,check => check === 'Jora'));
+
+function every(arr,callback){
+    for(let i = 0;i < arr.length ; i++){
+        if (!callback(arr[i],i,arr)) return false;
     }
+
+    return true
 }
 
-function find(callback){
-    
-    return callback(element,index,array)
-}
 
-cb('Jora',3,arr);
-console.log(find(cb))
+const arr1 = [20,30,40,40,60];
 
-// console.log(cb('Jora',3,arr));
-// console.log(cb('Dick',7,arr));
-// console.log(cb('Tom',3,arr))
+console.log(every([2,2,3,4],item => item > 1));
+console.log(every([20,30,40,40,60],item => item > 10));
+console.log(every([20,30,40,40,2],item => item > 10));
 
-// find()
+// function forEach(arr,callback){
+//     for (let i = 0; i < arr.length; i++){
+//         return callback(arr[i],i,arr);
+//     }
+// }
+
+// console.log(forEach(arr, item => item**2))
